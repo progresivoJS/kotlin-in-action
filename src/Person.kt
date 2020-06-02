6ee9564c8654b75135ade884e88f7b91a9f23f57
jinseong.hwang <jinseong.hwang@linecorp.com>
2020-06-02 23:15:41 +0900

    Class

---
+++ src/Person.kt
@@ -0,0 +1,21 @@
+class Person(
+        // private field, public getter
+        val name: String,
+
+        // private field, public getter/setter
+        // getter: isMarried
+        // setter: setMarried (예외 - is*)
+        var isMarried: Boolean
+)
+
+fun main() {
+    val js = Person("progresivojs", false)
+
+    // this statement actually calls getter
+    // js.getName()
+    println(js.name)
+
+    // this statement actually calls setter
+    // js.setMarried(true)
+    js.isMarried = true
+}
