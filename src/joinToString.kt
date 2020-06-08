4ee1835872625a4ad15e8c45f3edaf5149f32ad9
jinseong.hwang <jinseong.hwang@linecorp.com>
2020-06-08 23:34:26 +0900

    확장함수

--- src/joinToString.kt
+++ src/joinToString.kt
@@ -13,8 +13,31 @@ fun <T> joinToString(
     return result.toString()
 }
 
+fun <T> Collection<T>.stringJoin(
+        separator: String = ", ",
+        prefix: String = "",
+        postfix: String = ""
+): String {
+    val result = StringBuilder(prefix)
+    for ((index, element) in this.withIndex()) {
+        if (index > 0) result.append(separator)
+        result.append(element)
+    }
+    result.append(postfix)
+    return result.toString()
+}
+
+fun Collection<String>.join(
+        separator: String = ", ",
+        prefix: String = "",
+        postfix: String = ""
+) = joinToString(separator, prefix, postfix)
+
 fun main() {
     val list = listOf(1, 2, 3)
     println(joinToString(list, "; ", "(", ")"))
     println(joinToString(list, separator = " ", prefix = " ", postfix = "."))
+
+    println(list.stringJoin("; ", "(", ")"))
+    println(listOf("one", "two", "eight").join())
 }
--- src/strings/join.kt
+++ src/strings/join.kt
@@ -1,5 +1,13 @@
 package strings
 
 fun joinToString() {
-    
+
+}
+
+fun String.lastChar(): Char = this.get(this.length - 1)
+fun String.lastCharWithoutThis(): Char = get(length - 1)
+
+fun main() {
+    println("Kotlin".lastChar())
+    println("Kotlin".lastCharWithoutThis())
 }
