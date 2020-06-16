82f6a0c262b2a9ef8d72339f895736d9317d59a7
jinseong.hwang <jinseong.hwang@linecorp.com>
2020-06-16 23:52:13 +0900

    regex

---
+++ src/regex.kt
@@ -0,0 +1,12 @@
+fun parsePath(path: String) {
+    val regex = """(.+)/(.+)\.(.+)""".toRegex()
+    val matchResult = regex.matchEntire(path)
+    if (matchResult != null) {
+        val (directory, filename, extension) = matchResult.destructured
+        println("Dir: $directory, name: $filename, ext: $extension")
+    }
+}
+
+fun main() {
+    parsePath("/Users/yole/kotlin-book/chapter.adoc")
+}
